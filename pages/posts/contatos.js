926e350a8ea90db24bd265724d9d152b582c28fc
Gabriel dos Santos Oliveira <86084272+GabrieldosSantosOliveira@users.noreply.github.com>
2022-05-26 16:03:31 +0000

    Feito melhorias

--- pages/posts/contatos.js
+++ pages/posts/contatos.js
@@ -1,7 +1,4 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../../styles/Home.module.css'
-import Link from 'next/link'
 import Menu from '../../components/Menu'
 import Footer from '../../components/Footer'
 import Mean from '../../components/Mean'
