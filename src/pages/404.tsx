97d4f7ffb59280201fbaf6311d903fa3d8548549
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-04-13 11:24:30 -0400

    add padding in heading page not found

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -9,7 +9,7 @@ export default function NotFound() {
       </Head>
       <main className="gap-5 flex-col items-center justify-center flex py-20 min-h-screen min-w-full bg-background animate-background bg-300">
         <h1 className="font-bold fon-sans text-white text-9xl">404</h1>
-        <Heading level="h2">
+        <Heading level="h2" className="text-center px-5">
           Não foi possível encontrar o que voce queria
         </Heading>
       </main>
